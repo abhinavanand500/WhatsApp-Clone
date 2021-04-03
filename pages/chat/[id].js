fbad1eef2b8ada29fcde9d1690ef8ff32a07c7ca
Abhinav Anand <abhinav.anand500@gmail.com>
2021-04-03 18:06:51 +0530

    Chat CSS

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,16 +1,34 @@
 import React from 'react'
+import Sidebar from "../components/Sidebar"
 import Head from 'next/head'
 import styled from "styled-components";
+import ChatScreen from "../components/ChatScreen"
 const Chat = () => {
     return (
         <Container>
             <Head>
                 <title>Chat</title>
             </Head>
-            <h1>This is chat</h1>
+            <Sidebar/>
+            <ChatContainer>
+                <ChatScreen/>
+            </ChatContainer>
         </Container>
     )
 }
-
+ 
 export default Chat
-const Container = styled.div``;
+const Container = styled.div`
+    display:flex;
+`;
+const ChatContainer=styled.div`
+    flex:1;
+    overflow:scroll;
+    height : 100vh;
+
+    ::webkit-scrollbar{
+        display:none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width:none;
+`
